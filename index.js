3e475c3a9c5360004710164a31b5867d6bb48fd2
M-jo2 <73107310+M-jo2@users.noreply.github.com>
2024-08-02 09:31:25 +0200

    ok

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ function addRotation(element) {
     const randomRotation = rotationSet[Math.floor(Math.random() * rotationSet.length)];
     history[history.length - 1].rotation = randomRotation;
     element.style.transform = `rotate(${randomRotation}deg)`;
-    element.classList.add("rotation-animation")
-    element.style.setProperty('--rotate-angle', `${randomRotation}deg`);
+    element.firstElementChild.classList.add("rotation-animation")
+    element.firstElementChild.style.setProperty('--rotate-angle', `${randomRotation}deg`);
 }
 
 function addPosition(element) {
